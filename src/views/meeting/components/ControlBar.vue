13cda680d52fa122766d165665ecfaf3c9fc955a
mujl <1219757241@qq.com>
2024-04-13 01:21:21 +0800

    增加选择功能

--- src/views/meeting/components/ControlBar.vue
+++ src/views/meeting/components/ControlBar.vue
@@ -2,7 +2,7 @@
     <a-space-compact block>
         <a-dropdown placement="top">
             <a-button
-                :type="userMediaConstraints.audio ? 'primary' : 'default'"
+                :type="constraints.audio ? 'primary' : 'default'"
                 :disabled="disabled || audioInputs.length === 0"
                 @click="handleAudioClick"
             >
@@ -10,7 +10,10 @@
                 <div>麦克风</div>
             </a-button>
             <template #overlay>
-                <a-menu @click="handleAudioSelect">
+                <a-menu
+                    selectable
+                    v-model:selectedKeys="currentAudioInputs"
+                >
                     <a-menu-item
                         v-for="input of audioInputs"
                         :key="input.deviceId"
@@ -23,7 +26,7 @@
 
         <a-dropdown placement="top">
             <a-button
-                :type="userMediaConstraints.video ? 'primary' : 'default'"
+                :type="constraints.video ? 'primary' : 'default'"
                 :disabled="disabled || videoInputs.length === 0"
                 @click="handleVideoClick"
             >
@@ -31,7 +34,10 @@
                 <div>视频</div>
             </a-button>
             <template #overlay>
-                <a-menu @click="handleVideoSelect">
+                <a-menu
+                    selectable
+                    v-model:selectedKeys="currentVideoInputs"
+                >
                     <a-menu-item
                         v-for="input of videoInputs"
                         :key="input.deviceId"
@@ -71,7 +77,6 @@ import {
     VideoCameraOutlined,
 } from '@ant-design/icons-vue';
 import { useDevicesList, useDisplayMedia, useUserMedia } from '@vueuse/core';
-import type { MenuProps } from 'ant-design-vue';
 import { computed, inject, ref, watch } from 'vue';
 
 import PEER_MAP_CTRL from './peerProvider';
@@ -85,75 +90,65 @@ const disabled = computed<boolean>(
 const { videoInputs, audioInputs } = useDevicesList({
     requestPermissions: true,
 });
+const currentAudioInputs = ref<string[]>([]);
+const currentVideoInputs = ref<string[]>([]);
 
 // 本地流控制
-const userMediaConstraints = ref<MediaStreamConstraints>({
-    video: false,
-    audio: false,
+const constraints = computed<MediaStreamConstraints>(() => ({
+    audio:
+        currentAudioInputs.value.length > 0
+            ? { advanced: [{ deviceId: currentAudioInputs.value[0] }] }
+            : false,
+    video:
+        currentVideoInputs.value.length > 0
+            ? { advanced: [{ deviceId: currentVideoInputs.value[0] }] }
+            : false,
+}));
+const { stream: userStream, ...userStreamCtrl } = useUserMedia({ constraints });
+const { stream: displayStream, ...displayMediaCtrl } = useDisplayMedia({
+    video: true,
+    audio: true,
 });
-const userMediaCtrl = useUserMedia({ constraints: userMediaConstraints });
-const displayMediaCtrl = useDisplayMedia({ video: true, audio: true });
 
 // 更新本地流
+watch(
+    constraints,
+    (newConstraints, oldConstraints) => {
+        if (!oldConstraints.audio && !oldConstraints.video) {
+            userStreamCtrl.start();
+        } else if (!newConstraints.audio && !newConstraints.video) {
+            userStreamCtrl.stop();
+        } else {
+            userStreamCtrl.restart();
+        }
+    },
+    { deep: true },
+);
 function streamChangeHandler(newStream?: MediaStream, oldStream?: MediaStream) {
     if (!peerMapCtrl) return;
     peerMapCtrl.changeStream(newStream, oldStream);
 }
-watch(() => userMediaCtrl.stream.value, streamChangeHandler);
-watch(() => displayMediaCtrl.stream.value, streamChangeHandler);
+watch(userStream, streamChangeHandler);
+watch(displayStream, streamChangeHandler);
 
 function handleAudioClick() {
-    const audio = userMediaConstraints.value.audio;
-    userMediaConstraints.value.audio = !audio;
-    if (userMediaCtrl.stream.value) {
-        if (audio) {
-            userMediaCtrl.stop();
-        } else {
-            userMediaCtrl.restart();
-        }
+    if (currentAudioInputs.value.length === 0 && audioInputs.value.length > 0) {
+        currentAudioInputs.value = [audioInputs.value[0].deviceId];
     } else {
-        userMediaCtrl.start();
+        currentAudioInputs.value = [];
     }
 }
-
-const handleAudioSelect: MenuProps['onClick'] = ({ key }) => {
-    userMediaConstraints.value.audio = {
-        advanced: [{ deviceId: key as string }],
-    };
-    if (userMediaCtrl.stream.value) {
-        userMediaCtrl.restart();
-    } else {
-        userMediaCtrl.start();
-    }
-};
 
 function handleVideoClick() {
-    const video = userMediaConstraints.value.video;
-    userMediaConstraints.value.video = !video;
-    if (userMediaCtrl.stream.value) {
-        if (video) {
-            userMediaCtrl.stop();
-        } else {
-            userMediaCtrl.restart();
-        }
+    if (currentVideoInputs.value.length === 0 && videoInputs.value.length > 0) {
+        currentVideoInputs.value = [videoInputs.value[0].deviceId];
     } else {
-        userMediaCtrl.start();
+        currentVideoInputs.value = [];
     }
 }
 
-const handleVideoSelect: MenuProps['onClick'] = ({ key }) => {
-    userMediaConstraints.value.video = {
-        advanced: [{ deviceId: key as string }],
-    };
-    if (userMediaCtrl.stream.value) {
-        userMediaCtrl.restart();
-    } else {
-        userMediaCtrl.start();
-    }
-};
-
 function handleScreenClick() {
-    if (displayMediaCtrl.stream.value) {
+    if (displayStream.value) {
         displayMediaCtrl.stop();
     } else {
         displayMediaCtrl.start();
